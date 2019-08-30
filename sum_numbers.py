d47300eb885e452df0da91a08a530917438cd475
Steven Murray <tstevenm@gmail.com>
2019-08-30 14:06:29 -0500

    Initial Commit

---
+++ sum_numbers.py
@@ -0,0 +1,7 @@
+numbers = [1, 4, 8, 12, 8, 99, 54, 33, 101, 41, 66, 79, 85, 90, 92, 6]
+
+total = 0
+for number in numbers:
+    total += number
+
+print(f"Total of the numbers in the list: {total}")
